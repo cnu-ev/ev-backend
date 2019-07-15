ae2dc64276396f823ae7af2ef6388e2ad337986e
rinha7 <eocjstnals12@gmail.com>
2019-07-15 22:18:11 +0900

    임시로 url 받는 부분을 작성. 파이썬 데이터 가공부분과 post를 주고받을 url을 입력하면 끝.

--- ev/changer/urls.py
+++ ev/changer/urls.py
@@ -3,5 +3,6 @@ from .import views
 
 urlpatterns =[
     path('',views.index, name='index'),
-    path('/<int:id>/')
+    path('<int:question_id>', views.changer , name='change'),
+    path('number',views.number,name='number'),
 ]
